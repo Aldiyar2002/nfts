465d64eb2393e79f72d0a220b60a43d07752f6cc
Aldiyar2002 <80236018+Aldiyar2002@users.noreply.github.com>
2022-09-25 11:34:10 +0600

    nftss

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xa44d1738b854ec1611d1ce10a013d9ca2a1174bb";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
